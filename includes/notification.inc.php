a9b2de45b28c8f6df2bd38de2a802fe13eb485b3
bocqu <bocqu@PC-Port-MSI-Thomas>
2018-12-19 19:58:36 +0100

    Ajout de commentaires

--- includes/notification.inc.php
+++ includes/notification.inc.php
@@ -1,13 +1,18 @@
 <?php
-    if(isset($_SESSION['notifications'])) {
-        if($_SESSION['notifications']['result']==true) {
+    //afficher une notification
+    if(isset($_SESSION['notifications'])) { //si une notification est enregistrée dans les variables de session
+        if($_SESSION['notifications']['result']==true) { //on regarde quelle couleur donner à la notification
             $color_notification = "success";
         }
         else{
             $color_notification = "danger";
         }
+        
+        //transmettre à Smarty les données des notifications (couleur, conteu notification)
         $smarty->assign('color_notification', $color_notification);
         $smarty->display("notifications.tpl");
+        
+        //on supprime la notification (ne sera plus affichée après rafraichissement de la page)
         unset($_SESSION['notifications']);
 }
 
