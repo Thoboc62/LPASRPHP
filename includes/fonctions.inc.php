2524894078ab2133a02f5a41306c86154198fe20
bocqu <bocqu@PC-Port-MSI-Thomas>
2018-12-19 19:53:37 +0100

    Ajout de commentaires

--- includes/fonctions.inc.php
+++ includes/fonctions.inc.php
@@ -1,21 +1,24 @@
 <?php
 
+    //fonction permettant de hacher la chaine de caractères en paramètre
     function cryptPassword($mdp) {
         $mdp_crypt = sha1($mdp);
         return $mdp_crypt;
     }
 
+    //fonction permettant la génération d'un SID
     function sid($email) {
         $sid = md5($email.time());
         return $sid;
     }
 
-    //Fonction de retour d'index pour la pagination
+    //Fonction de retour d'index pour la pagination (article de départ de la sélection)
     function getIndex($page_courante, $nb_articles_par_page) {
         $index = ($page_courante-1)*$nb_articles_par_page;
         return $index;
     }
 
+    //fonction retournant le nombre d'articles publiés
     function nbTotalArticlesPublie($bdd) {
         /* @var $bdd PDO */
         $sql = "SELECT COUNT(*) AS nb_total_article_publie FROM articles WHERE publie = 1";
@@ -25,6 +28,7 @@
         return $tab_result['nb_total_article_publie'];
     }
 
+    //fonction retournant le nombre d'articles correspondant au motif de recherche
     function nbResultatsRecherche($bdd, $recherche) {
         /* @var $bdd PDO */
         $sql = "SELECT count(*) as nb_resultats FROM articles WHERE titre LIKE :titre OR texte LIKE :texte AND publie=1";
