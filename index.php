f0b12e600386fab9eafe2c32207e9da979826b45
bocqu <bocqu@PC-Port-MSI-Thomas>
2018-12-19 19:48:35 +0100

    Ajout de commentaires

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+        /*
+         * Blog LP ASR Dev. PHP
+         * Auteur: Thomas BOCQUELET
+         */
         session_start();
 
         require_once "config/init.conf.php";
@@ -7,29 +12,30 @@
         include_once "includes/head.inc.php";
         include_once "includes/fonctions.inc.php";
         
-        $nom_page = "index";
+        //Variable nécessaire pour la mise en surbrillance du terme dans la barre de menus
+            $nom_page = "index";
         
         //Mise en place de Smarty
         
-        require_once "libs/Smarty.class.php";
-        $smarty = new Smarty();
+            require_once "libs/Smarty.class.php";
+            $smarty = new Smarty();
 
-        $smarty->setTemplateDir("templates/");
-        $smarty->setCompileDir("templates_c/");
-       
-        $smarty->debugging = true;
+            $smarty->setTemplateDir("templates/");
+            $smarty->setCompileDir("templates_c/");
+
+            $smarty->debugging = true;
              
         //Insertion de la barre supérieure (menus)
-        include_once "includes/menu.inc.php";
+            include_once "includes/menu.inc.php";
         
         //Affichage de la notification, si besoin
-        include_once 'includes/notification.inc.php';
+            include_once 'includes/notification.inc.php';
 
         //Affichage des articles par page (pagination)
         
         $page_courante = empty($_GET['p']) ? 1 : $_GET['p']; //obtention numéro de page courante
 
-        $index = getIndex($page_courante, _NB_ART_PAR_PAGE); //détermination de l'index
+        $index = getIndex($page_courante, _NB_ART_PAR_PAGE); //détermination de l'index, article de départ de l'affichage
 
         $nb_articles = nbTotalArticlesPublie($bdd);
         $nb_pages = ceil($nb_articles / _NB_ART_PAR_PAGE); //ceil() arrondit au nombre entier supérieur
@@ -43,7 +49,7 @@
         //Préparation de la requête
         $sth = $bdd->prepare($select_article);
 
-        //Sécuriser les paramètres, qui a comme valeur 1, et doit être un booléen
+        //Sécurisation ds paramètres
         $sth->bindValue(":publie", 1, PDO::PARAM_BOOL);
         $sth->bindValue(":nb_article_par_page", _NB_ART_PAR_PAGE, PDO::PARAM_INT);
         $sth->bindValue(":index", $index, PDO::PARAM_INT);
@@ -53,14 +59,15 @@
 
         //Association des enregistrements
         $tab_articles = $sth->fetchAll(PDO::FETCH_ASSOC); //fetAll récupère toutes les entrées de la BDD d'un coup
-        //print_r2($tab_articles);
-        
-        //Envoi des variables nécessaires pour afichage dans template
+
+        //Envoi des variables nécessaires pour affichage dans template avec Smarty
         
         $smarty->assign('nom_page', $nom_page);
         $smarty->assign('tab_articles', $tab_articles);
         $smarty->assign('nb_pages', $nb_pages);
         
+        //Affichage de la page
         $smarty->display("index.tpl");
 
         include 'includes/footer.inc.php';
+?>
