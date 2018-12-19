562ca975f3c993a70843e1f815ccfc18757d2790
bocqu <bocqu@PC-Port-MSI-Thomas>
2018-12-19 22:40:53 +0100

    Ajout déconnexion du site

---
+++ deconnexion.php
@@ -0,0 +1,18 @@
+<?php
+
+    session_start();
+    setcookie("sid", "deconnexion", -1); //on supprime le cookie qui maintient la connexion
+    //session_destroy();
+    
+    $notification = "Vous avez été déconnecté.";
+    $succes_notification = true;
+    
+    //enregistrement de la notification dans les variables de session
+    $_SESSION['notifications']['message'] = $notification;
+    $_SESSION['notifications']['result'] = $succes_notification;
+    
+    header('Location: index.php');
+    
+
+?>
+
